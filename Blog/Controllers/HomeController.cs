1574156ef28ed30445a51bd1728b7539830f2cfb
IvanShchegolkov <sheg.ivan@gmail.com>
2021-12-07 21:13:03 +0300

    Edit home/index

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -91,41 +91,41 @@ namespace Blog.Controllers
         }
 
 
-        private static void GetImageBinaryFromDb()
-        {
-            // получаем данные их БД
-            List<byte[]> iScreen = new List<byte[]>(); // сделав запрос к БД мы получим множество строк в ответе, поэтому мы их сможем загнать в массив/List
-            List<string> iScreen_format = new List<string>();
-            using (SqlConnection sqlConnection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=dbtest; User Id=sa; Password=pass"))
-            {
-                sqlConnection.Open();
-                SqlCommand sqlCommand = new SqlCommand();
-                sqlCommand.Connection = sqlConnection;
-                sqlCommand.CommandText = @"SELECT [screen], [screen_format] FROM [report] WHERE [id] = 1"; // наша запись в БД под id=1, поэтому в запросе "WHERE [id] = 1"
-                SqlDataReader sqlReader = sqlCommand.ExecuteReader();
-                byte[] iTrimByte = null;
-                string iTrimText = null;
-                while (sqlReader.Read()) // считываем и вносим в лист результаты
-                {
-                    iTrimByte = (byte[])sqlReader["screen"]; // читаем строки с изображениями
-                    iScreen.Add(iTrimByte);
-                    iTrimText = sqlReader["screen_format"].ToString().TrimStart().TrimEnd(); // читаем строки с форматом изображения
-                    iScreen_format.Add(iTrimText);
-                }
-                sqlConnection.Close();
-            }
-            // конвертируем бинарные данные в изображение
-            byte[] imageData = iScreen[0]; // возвращает массив байт из БД. Так как у нас SQL вернёт одну запись и в ней хранится нужное нам изображение, то из листа берём единственное значение с индексом '0'
-            MemoryStream ms = new MemoryStream(imageData);
-            Image newImage = Image.FromStream(ms);
+        //private static void GetImageBinaryFromDb()
+        //{
+        //    // получаем данные их БД
+        //    List<byte[]> iScreen = new List<byte[]>(); // сделав запрос к БД мы получим множество строк в ответе, поэтому мы их сможем загнать в массив/List
+        //    List<string> iScreen_format = new List<string>();
+        //    using (SqlConnection sqlConnection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=dbtest; User Id=sa; Password=pass"))
+        //    {
+        //        sqlConnection.Open();
+        //        SqlCommand sqlCommand = new SqlCommand();
+        //        sqlCommand.Connection = sqlConnection;
+        //        sqlCommand.CommandText = @"SELECT [screen], [screen_format] FROM [report] WHERE [id] = 1"; // наша запись в БД под id=1, поэтому в запросе "WHERE [id] = 1"
+        //        SqlDataReader sqlReader = sqlCommand.ExecuteReader();
+        //        byte[] iTrimByte = null;
+        //        string iTrimText = null;
+        //        while (sqlReader.Read()) // считываем и вносим в лист результаты
+        //        {
+        //            iTrimByte = (byte[])sqlReader["screen"]; // читаем строки с изображениями
+        //            iScreen.Add(iTrimByte);
+        //            iTrimText = sqlReader["screen_format"].ToString().TrimStart().TrimEnd(); // читаем строки с форматом изображения
+        //            iScreen_format.Add(iTrimText);
+        //        }
+        //        sqlConnection.Close();
+        //    }
+        //    // конвертируем бинарные данные в изображение
+        //    byte[] imageData = iScreen[0]; // возвращает массив байт из БД. Так как у нас SQL вернёт одну запись и в ней хранится нужное нам изображение, то из листа берём единственное значение с индексом '0'
+        //    MemoryStream ms = new MemoryStream(imageData);
+        //    Image newImage = Image.FromStream(ms);
 
-            // сохраняем изоражение на диск
-            string iImageExtension = iScreen_format[0]; // получаем расширение текущего изображения хранящееся в БД
-            string iImageName = @"C:\result_new" + "." + iImageExtension; // задаём путь сохранения и имя нового изображения
-            if (iImageExtension == "png") { newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Png); }
-            else if (iImageExtension == "jpg" || iImageExtension == "jpeg") { newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Jpeg); }
-            else if (iImageExtension == "gif") { newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Gif); }
-            // и т.д., можно все if заменить на одну строку "newImage.Save(iImageName)", насколько это правильно сказать не могу, но работает
-        }
+        //    // сохраняем изоражение на диск
+        //    string iImageExtension = iScreen_format[0]; // получаем расширение текущего изображения хранящееся в БД
+        //    string iImageName = @"C:\result_new" + "." + iImageExtension; // задаём путь сохранения и имя нового изображения
+        //    if (iImageExtension == "png") { newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Png); }
+        //    else if (iImageExtension == "jpg" || iImageExtension == "jpeg") { newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Jpeg); }
+        //    else if (iImageExtension == "gif") { newImage.Save(iImageName, System.Drawing.Imaging.ImageFormat.Gif); }
+        //    // и т.д., можно все if заменить на одну строку "newImage.Save(iImageName)", насколько это правильно сказать не могу, но работает
+        //}
     }
 }
