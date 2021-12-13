0c9886706222c14570ba1920669edcb0981d3296
IvanShchegolkov <sheg.ivan@gmail.com>
2021-12-13 22:44:38 +0300

    Edit project

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -16,7 +16,12 @@ using System.IO;
 using System.Data.SqlClient;
 using System.Data;
 using static System.Net.Mime.MediaTypeNames;
-
+using Newtonsoft.Json;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Media;
+//using Microsoft.AspNet.Identity;
 
 namespace Blog.Controllers
 {
@@ -65,16 +70,69 @@ namespace Blog.Controllers
             return View(await db.Post.ToListAsync());
         }
 
+        [Authorize]
+        public IActionResult Addpost()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Addpost(PostAddModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.PostTitle != null && model.PostText != null)
+                {
+                    var user = db.Person.Where(u => u.Login == User.Identity.Name).FirstOrDefault();
+                    Post post = new Post();
+                    post.PostText = model.PostText;
+                    post.Title = model.PostTitle;
+                    post.PersonId = user.Id;
+
+                    db.Post.Add(post);
+                    await db.SaveChangesAsync();
+
+                    if (model.PostImage != null)
+                    {
+                        byte[] imageData = null;
+                        using (var binaryReader = new BinaryReader(model.PostImage.OpenReadStream()))
+                        {
+                            imageData = binaryReader.ReadBytes((int)model.PostImage.Length);
+                        }
+                        Images images = new Images();
+                        images.ImageData = imageData;
+                        images.PostId = post.Id;
+                        images.FileName = model.PostImage.FileName;
+                        images.Title = model.PostImage.FileName;
+
+                        db.Images.Add(images);
+                        await db.SaveChangesAsync();
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Некорректные данные");
+                }
+            }
+            return View(model);
+        }
+
         [HttpPost]
         public async Task<IActionResult> UnAuthenticated()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             HttpContext.Response.Cookies.Delete(".AspNetCore.Cookies");
 
-            return Ok("success");
+            var jsonString = JsonConvert.SerializeObject("success");
+
+            return Ok(jsonString);
         }
 
-        public static byte[] ReadFile(string filePath)
+        private static byte[] ReadFile(string filePath)
         {
             byte[] buffer;
             FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
@@ -102,18 +160,51 @@ namespace Blog.Controllers
             var fileName = System.IO.Directory.GetCurrentDirectory() + @"\wwwroot\upload\img\";
             for (int i = 0; i < PostList.Count; i++)
             {
-                for (int j = 0; j < PostList[i].Images.Count; j++)
+                if (PostList[i].Images.Count > 0)
                 {
-                    PostList[i].Images[j].ImagePath = fileName + PostList[i].Images[j].FileName;
-                    if (!System.IO.File.Exists(PostList[i].Images[j].ImagePath) && PostList[i].Images[j].ImageData != null)
+                    for (int j = 0; j < PostList[i].Images.Count; j++)
                     {
-                        System.IO.File.WriteAllBytes(fileName, PostList[i].Images[j].ImageData);
-                        System.Diagnostics.Process.Start(fileName);
+                        if (PostList[i].Images[j].FileName != null || PostList[i].Images[j].FileName != "")
+                        {
+                            PostList[i].Images[j].ImagePath = fileName + PostList[i].Images[j].FileName;
+                            if (!System.IO.File.Exists(PostList[i].Images[j].ImagePath) && PostList[i].Images[j].ImageData != null)
+                            {
+                                System.IO.File.WriteAllBytes(PostList[i].Images[j].ImagePath, PostList[i].Images[j].ImageData);
+                                //System.Diagnostics.Process.Start(fileName);
+                            }
+                        }
                     }
                 }
             }
 
             return PostList;
+        }
+
+        private System.Drawing.Image CropImage(System.Drawing.Image sourceImage, int sourceX, int sourceY, int sourceWidth, int sourceHeight, int destinationWidth, int destinationHeight)
+        {
+            System.Drawing.Image destinationImage = new Bitmap(destinationWidth, destinationHeight);
+
+            using (Graphics g = Graphics.FromImage(destinationImage))
+                g.DrawImage(
+                  sourceImage,
+                  new Rectangle(0, 0, destinationWidth, destinationHeight),
+                  new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                  GraphicsUnit.Pixel
+                );
+
+            return destinationImage;
+        }
+
+        public System.Drawing.Image resizeImage(System.Drawing.Image imgToResize, Size size)
+        {
+            return (System.Drawing.Image)(new Bitmap(imgToResize, size));
+        }
+        public void MainTest()
+        {
+            string path = "C:\\Images\\img1.jpg";
+            System.Drawing.Image img = System.Drawing.Image.FromFile(path);
+            Bitmap imgbitmap = new Bitmap(img);
+            System.Drawing.Image resizedImage = resizeImage(imgbitmap, new Size(200, 200));
         }
     }
 }
