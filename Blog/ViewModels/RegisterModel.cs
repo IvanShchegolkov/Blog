4c9e4954182b5cbef1272217200433fe30ac4f48
IvanShchegolkov <sheg.ivan@gmail.com>
2022-02-04 10:26:29 +0300

    Add MS Identity

--- Blog/ViewModels/RegisterModel.cs
+++ Blog/ViewModels/RegisterModel.cs
@@ -20,4 +20,21 @@ namespace Blog.ViewModels
         [Required(ErrorMessage = "Не указан Email")]
         public string PersonEmail { get; set; }
     }
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердить пароль")]
+        public string PasswordConfirm { get; set; }
+    }
 }
